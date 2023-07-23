ac4f140845855bd50f23844b85414cd482082ea3
spicata <p.ixelandian@gmail.com>
2023-07-23 16:22:45 +0800

    err

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,32 +1,55 @@
+// activates when there is a key press (specifically when one is lifted)
+window.onload = redirect;
 document.onkeyup = KeyCheck;
 
-function leftarrowpressed() {
-    let current_url = location.href.substring(0, location.href.length - 1); // can you even do that?
-    console.log(current_url);
-    let current_num = location.href.substring(location.href.length - 1);
-    console.log(current_num);
-    current_num = parseInt(current_num)
-    location.href = current_url + (current_num - 1)
+// grabs the url from the... url
+function url_grabber() {
+    var current_url = location.href.split('#')[0];
+    var current_num = location.href.split('#')[1];
+    current_num = parseInt(current_num);
+    return [current_url, current_num];
 }
 
-function rightarrowpressed() {
-    let current_url = location.href.substring(0, location.href.length - 1); // can you even do that?
-    console.log(current_url);
-    let current_num = location.href.substring(location.href.length - 1);
-    console.log(current_num);
-    current_num = parseInt(current_num)
-    location.href = current_url + (current_num + 1)
+// adds or subtracts
+function previous_slide() {
+    let [current_url, current_num] = url_grabber();
+    if (current_num - 1 != 0) { 
+        location.href = current_url + '#' + (current_num - 1);
+    }
+}
+
+function next_slide() {
+    let [current_url, current_num] = url_grabber();
+    if (current_num + 1 <= document.querySelectorAll('img.xobdyn').length) {
+        location.href = current_url + '#' + (current_num + 1);
+    }
 } 
 
+// checks what key is pressed
 function KeyCheck(e) {
     var KeyID = (window.event) ? event.keyCode : e.keyCode;
 
     switch(KeyID) {
         case 188: // left
-            leftarrowpressed()
+            previous_slide()
             break;
         case 190: // right
-            rightarrowpressed()
+            next_slide()
             break;
     }
+}
+
+function redirect() {
+    if (location.href.includes('#') == false) {
+        location.href = location.href + '#1';
+    } else if (Math.floor(location.href.split('#')[1]) != location.href.split('#')[1]) {
+        location.href = location.href.split('#')[0] + '#1';
+        location.reload();
+    } else if (location.href.split('#')[1] == false) {
+        location.href = location.href.split('#')[0] + '#1';
+        location.reload();
+    } else if (location.href.split('#')[1].includes('.') == true) {
+        location.href = location.href.split('#')[0] + '#1';
+        location.reload();
+    } 
 }
